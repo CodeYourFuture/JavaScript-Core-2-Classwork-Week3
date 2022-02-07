4b29944e1320dcb16aac2ab0de45e1914151b82e
Alirezabg <91377914+Alirezabg@users.noreply.github.com>
2022-02-07 20:45:16 +0000

    London8-alireza-bagheri js2 w3

--- 1-exercises/exercise.js
+++ 1-exercises/exercise.js
@@ -21,16 +21,26 @@ Write JavaScript below that console.logs:
     4. all the "p" elements of contained inside the .primary-content element node
     --> should log a list of nodes with a length of 3
 
-*/
-
+// */
+let paragraphVar = document.querySelector("p");
+console.log(paragraphVar);
+let divElement = document.querySelector("div");
+console.log(divElement);
+let jumboId = document.querySelector("#jumbotronText");
+console.log(jumboId);
+let pElement = document.querySelectorAll(".primary-content p");
+console.log(pElement);
 
 /*
 Task 2
 ======
 
 When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
-*/
-
+// */
+let alertButton = document.querySelector(`#alertBtn`);
+alertButton.addEventListener("click", () => {
+  alert("Thanks for visiting Bikes for Refugees!");
+});
 
 /*
 Task 3
@@ -39,7 +49,18 @@ Task 3
 When a user clicks the 'Change colour' button, the background colour of the page should change to red.
 Hint: can you do this with a CSS class instead of adding styles to the element?
 */
+// let changeColour = document.querySelector(`#bgrChangeBtn`);
+// changeColour.addEventListener(`click`, () => {
+//   // document.body.style.backgroundColor = "red";
+//   document.body.classList.add(`background--red`);
+// });
+//  I made a mistake, used document in line 55 and use body directly
 
+let changeBgBtn = document.querySelector("#bgrChangeBtn");
+changeBgBtn.addEventListener("click", () => {
+  let body = document.querySelector("body");
+  body.classList.add("background--red");
+});
 
 /*
 Task 4
@@ -47,8 +68,22 @@ Task 4
 
 When a user clicks the 'Larger links!' button, the text of all links on the page should increase.
 */
+// let largerLinks = document.querySelector(`#largerLinksBtn`);
+// largerLinks.addEventListener(`click`, () => {
+//   let link = document.querySelectorAll(`a`);
+//   for (let lnk of link) {
+//     lnk.style.fontSize = "26px";
+//   }
+// });
+// the code above are those I wrote in the class and are correct.
 
-
+let largerLinksButton = document.querySelector("#largerLinksBtn");
+largerLinksButton.addEventListener("click", () => {
+  let links = document.querySelectorAll("a");
+  for (let link of links) {
+    link.style.fontSize = "25px";
+  }
+});
 /*
 Task 5
 ======
@@ -56,3 +91,21 @@ Task 5
 When a user clicks the 'Add some text' button, the text in the input field should be appended to the page as a new paragraph below it.
 Hint: the new paragraph should be appended to the element with id 'addArticle'.
 */
+// let addSomeText = document.querySelector(`#addArticleBtn`);
+// addSomeText.addEventListener(`click`,()=> {
+//  let textInput = document.querySelector(`#addArticleInput`).value ;
+//  let paragraph = document.createElement(`p`);
+//  paragraph.innerText = textInput.value;
+
+// })
+// I couldn't finish those code in the class work.
+
+let addTextBtn = document.querySelector("#addArticleBtn");
+addTextBtn.addEventListener("click", () => {
+  let input = document.querySelector("#addArticleInput");
+  let newParagraph = document.createElement("p");
+  newParagraph.innerText = input.value;
+  let parentElement = document.querySelector("#addArticle");
+  parentElement.appendChild(newParagraph);
+  input.value = ""; // resets the input field
+});
