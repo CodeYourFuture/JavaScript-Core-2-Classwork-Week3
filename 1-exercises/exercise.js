51b336cc41c3c966322320b29de482f055e61306
SanaAsaf <sanaasaf@gmail.com>
2023-01-20 19:23:46 +0000

    London 9 - Sana Asaf - JavaScript - Core - 2 - Coursework - Week3

--- 1-exercises/exercise.js
+++ 1-exercises/exercise.js
@@ -22,15 +22,27 @@ Write JavaScript below that console.logs:
     --> should log a list of nodes with a length of 3
 
 */
+const task1=document.querySelectorAll("p");
+console.log(task1)
+  
+const task2=document.querySelector(".site-header");
+console.log(task2)
 
+const task3=document.querySelector("#jumbotronText");
+console.log(task3)
 
+const task4=document.querySelector(".primary-content").querySelectorAll("p");
+//const task4=document.querySelectorAll(".primary-content p");
+console.log(task4)
 /*
 Task 2
 ======
 
 When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
 */
-
+const myButton= document.querySelector("#alertBtn");
+myButton.addEventListener("click",()=>{
+alert("Thanks for visiting Bikes for Refugees!")});
 
 /*
 Task 3
@@ -39,6 +51,11 @@ Task 3
 When a user clicks the 'Change colour' button, the background colour of the page should change to red.
 Hint: can you do this with a CSS class instead of adding styles to the element?
 */
+const backgroundButton = document.querySelector("#bgrChangeBtn");
+backgroundButton.addEventListener("click",()=>{
+ // const bdyelement = document.querySelector
+backgroundButton.style.backgroundColor="red"});
+
 
 
 /*
@@ -48,7 +65,16 @@ Task 4
 When a user clicks the 'Larger links!' button, the text of all links on the page should increase.
 */
 
-
+const largerLinks = document.querySelector("#largerLinksBtn");
+largerLinks.addEventListener("click",()=>{
+  let links=document.querySelectorAll("a")
+  //console.log(links);
+ //link.forEach (tag =>{tag.style.fontsize="x-larger"})
+  for (let i=0;i<links.length;i++){
+  links[i].style.fontSize="32px";
+    
+  }
+}); 
 /*
 Task 5
 ======
@@ -56,3 +82,19 @@ Task 5
 When a user clicks the 'Add some text' button, the text in the input field should be appended to the page as a new paragraph below it.
 Hint: the new paragraph should be appended to the element with id 'addArticle'.
 */
+/*const newParagraphElement = document.createElement("p")
+newParagraphElement.innerText = "Hey, thanks for visiting!"
+newParagraphElement.className = "row"
+
+const footerElement = document.querySelector("#navbarSupportedContent")
+footerElement.appendChild(newParagraphElement)
+*/
+const addtext= document.querySelector("#addArticleBtn")
+addtext.addEventListener("click",()=>{
+const newParaElement = document.createElement("p")
+const newInput = document.querySelector("#addArticleInput")
+const inputValue = newInput.value
+newParaElement.innerText=inputValue
+const footerElement = document.querySelector("#addArticle")
+footerElement.appendChild(newParaElement)
+})
